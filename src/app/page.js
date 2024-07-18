f5c762b48432472aba9909cb4b06a631faab86cb
Juan Nunez <167369316+JuanDiegoNunez@users.noreply.github.com>
2024-07-18 00:58:15 +0000

    todoList finish

--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex-col">
-      <div className='container mx-auto text-2xl'>Hello</div>
-    </main>
-  );
-}
---
+++ src/app/todo/page.js
@@ -0,0 +1,9 @@
+import { TodoList } from "../components/TodoList";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex-col">
+      <TodoList user = "user2"/>
+      </main>
+  );
+}
